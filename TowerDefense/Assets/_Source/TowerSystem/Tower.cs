14eba5a8093187b61afad72b0d47c49816245c7a
Viwka <vwko111@gmail.com>
2023-10-25 18:04:22 +0300

    feat -  tower aiming/shooting

--- TowerDefense/Assets/_Source/TowerSystem/Tower.cs
+++ TowerDefense/Assets/_Source/TowerSystem/Tower.cs
@@ -9,21 +9,48 @@ public class Tower : MonoBehaviour
     [Header("References")]
     [SerializeField] private LayerMask enemyMask;
     [SerializeField] private Transform towerRotationPoint;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
+
     [Header("Attribute")]
     [SerializeField] private float attackRange = 5f;
+    [SerializeField] private float rotationSpeed = 5f;
+    [SerializeField] private float fireRate = 1f;
 
 
 
     private Transform target;
+    private float timeUntilFire;
 
     private void Update()
     {
         if (target == null)
         {
             FindTarget();
+            return;
         }
 
         RotateTowardsTarget();
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+        else
+        {
+            timeUntilFire += Time.deltaTime;
+            if (timeUntilFire >= 1f / fireRate)
+            {
+                Shoot();
+                timeUntilFire = 0f;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bulletObj = Instantiate(bulletPrefab,firingPoint.position,Quaternion.identity);
+        Bullet bulletScript = bulletObj.GetComponent<Bullet>();
+        bulletScript.SetTarget(target);
     }
 
     private void FindTarget()
@@ -35,12 +62,16 @@ public class Tower : MonoBehaviour
             target = hits[0].transform;
         }
     }
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(target.position, transform.position) <= attackRange;
+    }
     private void RotateTowardsTarget()
     {
-        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
 
-        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f,0f,angle));
-        towerRotationPoint.rotation = targetRotation;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        towerRotationPoint.rotation = Quaternion.RotateTowards(towerRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 
     private void OnDrawGizmosSelected()
